b9696c6e73f13e44d4efb550d85a3db21bf9c2f4
raghaves <raghaves@microsoft.com>
2020-07-06 17:17:04 +0530

    Changes

--- .github/workflows/script.sh
+++ .github/workflows/script.sh
@@ -11,7 +11,7 @@ prNumber=$4
 frombranch=$5
 tobranch=$6
 
-haeder="Authorization: token ${token}"
+header="Authorization: token $token"
 echo ">> $header"
 
 response=$(curl -X POST -H "Authorization: token $token" https://github.com/rgsubh/l2-integration-tests --data '{"event_type": "K8sCreateSecretReleasesPR", "client_payload": {"action": "CreateSecret", "commit": "$commit", "repository":  "${{ github.repository }}", "prNumber": "${{ github.event.pull_request.number }}", "tobranch": "${{ github.event.pull_request.base.ref }}",  "frombranch": "${{ github.event.pull_request.head.ref }}" }}')
