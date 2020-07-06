d2024ab04d5bf9fb0d37703570f6bf0b00782d4c
raghaves <raghaves@microsoft.com>
2020-07-06 17:40:35 +0530

    Changes

--- .github/workflows/script.sh
+++ .github/workflows/script.sh
@@ -13,11 +13,35 @@ tobranch=$6
 
 header="Authorization: token $token"
 echo ">> $header"
+echo ">>>> "$(getHeader)""
 
-response=$(curl -X POST -H $header https://github.com/rgsubh/l2-integration-tests --data '{"event_type": "K8sCreateSecretReleasesPR", "client_payload": {"action": "CreateSecret", "commit": "$commit", "repository":  "${{ github.repository }}", "prNumber": "${{ github.event.pull_request.number }}", "tobranch": "${{ github.event.pull_request.base.ref }}",  "frombranch": "${{ github.event.pull_request.head.ref }}" }}')
+response=$(curl -X POST -H $header "https://github.com/rgsubh/l2-integration-tests" --data "$(getPayLoad)")
 if [ "$response" == "" ]; then
     echo "Integration tests triggered successfully"
 else
     echo "Triggering integration tests failed with: '$response'"
     exit 1
 fi
+
+getHeader(){
+    cat <<EOF
+    "Authorization: token $token"
+EOF
+}
+
+getPayLoad() {
+    cat <<EOF
+{
+    "event_type": "K8sCreateSecretReleasesPR", 
+    "client_payload": 
+    {
+        "action": "CreateSecret", 
+        "commit": "$commit", 
+        "repository": "$repository", 
+        "prNumber": "$prNumber", 
+        "tobranch": "$tobranch", 
+        "frombranch": "$frombranch"
+    }
+}
+EOF
+}
