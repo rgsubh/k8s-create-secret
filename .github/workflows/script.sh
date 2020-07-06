677b67d63ed1f69458e2a664f66ffe8439979c69
raghaves <raghaves@microsoft.com>
2020-07-06 17:13:51 +0530

    Changes

--- .github/workflows/script.sh
+++ .github/workflows/script.sh
@@ -11,6 +11,9 @@ prNumber=$4
 frombranch=$5
 tobranch=$6
 
+haeder="Authorization: token ${token}"
+echo ">> $header"
+
 response=$(curl -X POST -H "Authorization: token $token" https://github.com/rgsubh/l2-integration-tests --data '{"event_type": "K8sCreateSecretReleasesPR", "client_payload": {"action": "CreateSecret", "commit": "$commit", "repository":  "${{ github.repository }}", "prNumber": "${{ github.event.pull_request.number }}", "tobranch": "${{ github.event.pull_request.base.ref }}",  "frombranch": "${{ github.event.pull_request.head.ref }}" }}')
 if [ "$response" == "" ]; then
     echo "Integration tests triggered successfully"
