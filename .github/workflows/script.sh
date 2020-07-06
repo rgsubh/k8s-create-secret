0629ed3026b248f4f3f57ed26a848e9ff0c50965
raghaves <raghaves@microsoft.com>
2020-07-06 17:53:15 +0530

    Changes

--- .github/workflows/script.sh
+++ .github/workflows/script.sh
@@ -1,19 +1,9 @@
-echo "Arg: $1"
-echo "Arg: $2"
-echo "Arg: $3"
-echo "Arg: $4"
-echo "Arg: $5"
-
 token=$1
 commit=$2
 repository=$3
 prNumber=$4
 frombranch=$5
 tobranch=$6
-
-header="Authorization: token $token"
-echo ">> $header"
-echo "<><><> "$(getPayLoad)""
 
 getPayLoad() {
     cat <<EOF
@@ -32,7 +22,12 @@ getPayLoad() {
 EOF
 }
 
-response=$(curl -X POST -H "$header" https://github.com/rgsubh/l2-integration-tests --data "$(getPayLoad)")
+header="Authorization: token $token"
+echo ">> $header"
+echo "<><><> "$(getPayLoad)""
+
+
+response=$(curl -X POST -H "$header" https://github.com/rgsubh/l2-integration-tests --data "$(getPayLoad)" )
 if [ "$response" == "" ]; then
     echo "Integration tests triggered successfully"
 else
