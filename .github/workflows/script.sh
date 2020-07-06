85daaec5c3721509cce888083e83457009ea6665
raghaves <raghaves@microsoft.com>
2020-07-06 18:47:49 +0530

    Changes

--- .github/workflows/script.sh
+++ .github/workflows/script.sh
@@ -28,7 +28,7 @@ getHeader() {
 EOF
 }
 
-response=$(curl -X POST -H "$(getHeader)" https://api.github.com/repos/rgsubh/l2-integration-tests/dispatches --data "$(getPayLoad)")
+response=$(curl -X POST -H "$(getHeader)" https://api.github.com/repos/rgsubh/l2-integration-tests/dispatches -d "$(getPayLoad)")
 
 if [ "$response" == "" ]; then
     echo "Integration tests triggered successfully"
