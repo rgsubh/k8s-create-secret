198c8ecb189c7399250b4a89684311300d469d3d
raghaves <raghaves@microsoft.com>
2020-07-06 17:04:14 +0530

    Changes

--- .github/workflows/script.sh
+++ .github/workflows/script.sh
@@ -4,3 +4,17 @@ echo "Arg: $3"
 echo "Arg: $4"
 echo "Arg: $5"
 
+token = $1
+commit = $2
+repository = $3
+prNumber = $4
+frombranch = $5
+tobranch = $6
+
+response=$(curl -X POST -H "Authorization: token $token" https://github.com/rgsubh/l2-integration-tests --data '{"event_type": "K8sCreateSecretReleasesPR", "client_payload": {"action": "CreateSecret", "commit": "$commit", "repository":  "${{ github.repository }}", "prNumber": "${{ github.event.pull_request.number }}", "tobranch": "${{ github.event.pull_request.base.ref }}",  "frombranch": "${{ github.event.pull_request.head.ref }}" }}')
+if [ "$response" == "" ]; then
+    echo "Integration tests triggered successfully"
+else
+    echo "Triggering integration tests failed with: '$response'"
+    exit 1
+fi
